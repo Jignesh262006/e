7b0163cabbe434c8cf15336d490f2412667488c4
Asad Ali <[asad.ar451@gmail.com]>
2025-01-08 15:41:50 +0500

    add footer component with responsive design and icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {TopBarComponent} from './components/top-bar/top-bar.component';
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   imports: [
     RouterOutlet,
-    TopBarComponent
-  ],
+    TopBarComponent,
+    FooterComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
