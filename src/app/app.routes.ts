2e18b9dae54f216e2ee8932ebbe4e9f1094c044c
Rafay Muhammad <126745296+ara33n@users.noreply.github.com>
2025-01-08 11:56:13 +0500

    add home component and related assets

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,15 @@
 import { Routes } from '@angular/router';
-import {SidebarComponent} from './components/sidebar/sidebar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
 
 export const routes: Routes = [
-  {
-    path:'sidebar',
-    component:SidebarComponent
-  }
+    {
+        path: '',
+        component: HomeComponent
+    },
+    {
+        path: 'sidebar',
+        component: SidebarComponent
+    }
 ];
