d7b1a4d15493d0994501b68887b71cb362f3e13a
waseemahmad <waseemahmedofficial2000@gmail.com>
2025-01-08 10:25:55 +0500

    added top bar

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,24 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'primary': '#002982',
+        'secondary': '#002981',
+
+      },
+      spacing: {
+        '120': '120px',
+      },
+      fontSize: {
+        '64': '64px',
+      },
+      lineHeight: {
+        '76': '76px',
+      }
+    },
   },
   plugins: [],
 }
-
