4fc4eb3a2c09cb61b18c3253c321dff18076a577
Rafay Muhammad <126745296+ara33n@users.noreply.github.com>
2025-01-10 11:01:50 +0500

    add under-development component; implement basic structure and testing update home component animations; replace 'animate-ping' with 'animate-pingg' for improved visual effects

---
+++ src/app/components/under-development/under-development.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-under-development',
+  imports: [],
+  templateUrl: './under-development.component.html',
+  styleUrl: './under-development.component.css'
+})
+export class UnderDevelopmentComponent {
+
+}
